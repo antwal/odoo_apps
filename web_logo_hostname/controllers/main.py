017e95b8a6d619837988936ea7e7bbb9cfe5b6c7
Walter Antolini <antwal@gmail.com>
2016-02-01 21:44:21 +0100

    web_logo_hostname - Store version 1.0.5

---
+++ web_logo_hostname/controllers/main.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Antolini Walter (http://www.antwal.name)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+import functools
+from cStringIO import StringIO
+
+import openerp
+from openerp import http
+from openerp.http import request
+from openerp.modules import get_module_resource
+
+db_monodb = http.db_monodb
+
+class Binary(openerp.addons.web.controllers.main.Binary):
+
+    @http.route([
+        '/web/binary/company_logo',
+        '/logo',
+        '/logo.png',
+    ], type='http', auth="none", cors="*")
+    def company_logo(self, dbname=None, **kw):
+        imgname = 'logo.png'
+        placeholder = functools.partial(get_module_resource, 'web', 'static', 'src', 'img')
+        uid = None
+        if request.session.db:
+            dbname = request.session.db
+            uid = request.session.uid
+        elif dbname is None:
+            dbname = db_monodb()
+
+        if not uid:
+            uid = openerp.SUPERUSER_ID
+
+        if not dbname:
+            response = http.send_file(placeholder(imgname))
+        else:
+            http_host = request.httprequest.environ.get("HTTP_HOST")
+            server_port = request.httprequest.environ.get("SERVER_PORT")
+
+            # wsgi test
+            if http_host:
+                hostname = http_host.replace(':'+server_port, '')
+
+            try:
+                # create an empty registry
+                registry = openerp.modules.registry.Registry(dbname)
+                with registry.cursor() as cr:
+                    if hostname:
+                        cr.execute("""SELECT c.logo_web, c.write_date
+                                        FROM res_company c
+                                       WHERE c.hostname = %s
+                               """, (hostname,))
+                    else:
+                        cr.execute("""SELECT c.logo_web, c.write_date
+                                        FROM res_users u
+                                   LEFT JOIN res_company c
+                                          ON c.id = u.company_id
+                                       WHERE u.id = %s
+                                   """, (uid,))
+                    row = cr.fetchone()
+                    if row and row[0]:
+                        image_data = StringIO(str(row[0]).decode('base64'))
+                        response = http.send_file(image_data, filename=imgname, mtime=row[1])
+                    else:
+                        response = http.send_file(placeholder('nologo.png'))
+            except Exception:
+                response = http.send_file(placeholder(imgname))
+
+        return response
